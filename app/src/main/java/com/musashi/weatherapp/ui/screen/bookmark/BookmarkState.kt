3502040d073880f24e1cbf221e4beb26e05e29dc
mu54omd <mu54omd@vivaldi.net>
2024-09-08 12:47:51 +0330

    fix drawable warning

--- app/src/main/java/com/musashi/weatherapp/ui/screen/bookmark/BookmarkState.kt
+++
@@ -1,2 +0,0 @@
-package com.musashi.weatherapp.ui.screen.bookmark
-
--- app/src/main/java/com/musashi/weatherapp/ui/screen/detailed/DetailedViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.musashi.weatherapp.ui.screen.detailed
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class DetailedViewModel @Inject constructor(): ViewModel()
